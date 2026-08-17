a96bfafb8bf9047041c3d54ac4b633d55483a512
annajeong0429-ux <annajeong0429@gmail.com>
2026-08-17 21:04:23 +0900

    fix: merge 리비전 down_revision 타입 힌트 수정 (mypy 실패 수정)

--- app/core/db/migrations/versions/8098fbd94c6e_merge_point_hold_care_cancel_and_.py
+++ app/core/db/migrations/versions/8098fbd94c6e_merge_point_hold_care_cancel_and_.py
@@ -14,7 +14,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "8098fbd94c6e"
-down_revision: Union[str, None] = ("f1a2b3c4d5e6", "f5a1c3e7b902")
+down_revision: Union[str, Sequence[str], None] = ("f1a2b3c4d5e6", "f5a1c3e7b902")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
